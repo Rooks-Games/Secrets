f4c9a184fd3b68421f1fc615cbd3d109fe03eebe
MuhammadIbrahim0 <64721208+MuhammadIbrahim0@users.noreply.github.com>
2024-11-30 16:28:23 +0500

    Made Prefabs

--- Assets/Scripts/DialogueSystem/DialogueUI/DialogueNpcUI.cs
+++ Assets/Scripts/DialogueSystem/DialogueUI/DialogueNpcUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class DialogueNpcUI : MonoBehaviour
 {
